8c0a7c75597fcf530a62817ad2b059141b132ddb
heline09 <luisashikuku@gmail.com>
2025-10-15 13:45:27 +0300

    Finalize week1 tasks

--- cypress/integrations/examples/Title.js
+++ cypress/integrations/examples/Title.js
@@ -1,6 +1,13 @@
+/// <reference types= "cypress" />
+
+
 describe('Basic Website Visit Test', () => {
   it('Visits the example website and checks the title', () => {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
     cy.title().should('include', 'GreenKart')
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.product:visible').should('have.length', 4)
+    
   })
 })
