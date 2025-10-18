21a9cbd168546db5ed5c5e1d20e2656806e8937d
heline09 <luisashikuku@gmail.com>
2025-10-18 14:34:05 +0300

    Implemented Page Object Model pattern

--- cypress/integrations/examples/EndToEnd.js
+++ cypress/integrations/examples/EndToEnd.js
@@ -1,39 +1,38 @@
-/// <reference types= "cypress" />
+import HomePage  from "../../support/pageObjects/Homepage"
 describe('End to End Ecommerce test', function () {
-    it('submit order', function (){
-        const productName = "Nokia Edge"
-        cy.visit("https://rahulshettyacademy.com/loginpagePractise/")
-        cy.get('#username').type("rahulshettyacademy")
-        cy.get('#password').type("learning")
-        cy.contains("Sign In").click()
-        cy.contains("Shop Name").should('be.visible')
-        cy.get('app-card').should('have.length', 4)
-        // goes through all cards to find Nokia Edge
-        cy.get('app-card').filter(`:contains("${productName}")`)
-        .then($element=>
-        {
-            cy.wrap($element).should('have.length', 1)
-            cy.wrap($element).contains('button','Add').click()
-        }
-        )
-        cy.get('app-card').eq(0).contains('button', 'Add').click() // 2 items added to the cart
-        cy.contains('a','Checkout').click()
-        let sum = 0
-        cy.get('tr td:nth-child(4) strong')
-        .each($el=>{
-            const amount = Number($el.text().split(" ")[1].trim())
-            sum = sum + amount //65000 after 1st iteration
+
+    before(function () {
+        // runs once before all tests in the block
+
+        cy.fixture('example').then(function (data) {
+            this.data = data // global variable
+            this.homepage = new HomePage()
         })
-        // comparison should be done after sum(forloop)
-        .then(function() {
+    })
+    it('submit order', function () {
+        const productName = this.data.productName
+
+        this.homepage.goTo("https://rahulshettyacademy.com/loginpagePractise/")
+
+        const productPage = this.homepage.login(this.data.username, this.data.password)
+        // cy.pause() // for debugging
+        productPage.pageValidation()
+        productPage.getCardCount().should('have.length', 4)
+        productPage.selectProduct(productName)
+        productPage.selectFirstProduct()
+        const cartPage = productPage.goToCart()
+        cartPage.sumOfProducts().then(function (sum) 
+        {
             expect(sum).to.be.lessThan(200000)
         })
-        cy.contains('button', 'Checkout').click()
-        
-        cy.get(".suggestions ul li a", { timeout: 10000 }).click()
-        cy.get('btn-success').click()
-        cy.get('alert-success').should('contain', 'Success')
-        
-        })
-        
+
+        const confirmPage = cartPage.checkoutItems()
+        confirmPage.submitFormDetails()
+        confirmPage.getAlertMessage().should('contain', 'Success')
+
+
+
+
+    })
+
 })
