80053b2c15724a5fa0c3deb64b3224d55861bd6f
heline09 <luisashikuku@gmail.com>
2025-10-22 15:57:09 +0300

    Added changes to ecommerce.feature

--- cypress/integration/BDD/ecommerce.feature
+++
@@ -1,8 +0,0 @@
-Feature: End to end ecommerce validation
-
-Scenario: Ecommerce products delivery
-Given I am on Ecommerce Page
-When I login in to the application
-And I add items to the Cart
-And Validate the total price limit
-Then Select the country submit and verify Thankyou
--- cypress/integration/examples/BDD/ecommerce/eCommStepDef.js
+++ cypress/integration/examples/BDD/ecommerce/eCommStepDef.js
@@ -1,4 +1,4 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor"
+import { Given, When, Then, DataTable } from "@badeball/cypress-cucumber-preprocessor"
 import HomePage from "../../../../support/pageObjects/Homepage"
 const homepage = new HomePage()
 
@@ -14,20 +14,23 @@ When('I log in to the application', function() {
 })
 
 When ('I log in to application portal', function(){
-    
+    this.productPage = homepage.login(dataTable.rawTable[1][0], dataTable.rawTable[1][1])
+    this.productPage.pageValidation()
+    this.productPage.getCardCount().should('have.length', 4)
 })
-When('I add items to the cart', function() {
+
+When('I add items to the Cart', function() {
     this.productPage.selectProduct(this.data.productName)
     this.productPage.selectFirstProduct()
     this.cartPage = this.productPage.goToCart()
 })
-When('I validate the total price limit', function(){
+When('Validate the total price limit', function(){
       this.cartPage.sumOfProducts().then(function (sum) {
             expect(sum).to.be.lessThan(200000)
         })
 })
-Then('select country submit and verify Thankyou', function(){
-    const confirmPage = cartPage.checkoutItems()
+Then('Select the country submit and verify Thankyou', function(){
+    const confirmPage = this.cartPage.checkoutItems()
     confirmPage.submitFormDetails()
     confirmPage.getAlertMessage().should('contain', 'Success')
 })
