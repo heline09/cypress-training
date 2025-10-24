13e263fdc673c155aba7e6934c683b035def4f84
heline09 <luisashikuku@gmail.com>
2025-10-24 15:09:45 +0300

    Final changes to command.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,6 +24,8 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
 Cypress.Commands.add("login", (username, password) => {
   cy.get('#username').type(username)
   cy.get('#password').type(password)
@@ -31,12 +33,11 @@ Cypress.Commands.add("login", (username, password) => {
 })
 
 Cypress.Commands.add("LoginAPI", () => {
-  cy.request("POST", "https://rahulshettyacademy.com/api/ecom/auth/login", {
+  cy.request("POST", "https://rahulshettyacademy.com/api/ecom/auth/login", {  
     userEmail: Cypress.env("userEmail"),
     userPassword: Cypress.env("userPassword")
-  }).
-    then(function (response) {
-      expect(response.status).to.eq(200)
-      Cypress.env('token', response.body.token);
-    })
+  }).then(function (response) {
+    expect(response.status).to.eq(200)
+    Cypress.env('token', response.body.token);
+  })
 })
